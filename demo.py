0952e0d987d1ddb0696a9ccd1486174071ebcd46
george <george030796@gmail.com>
2017-11-16 00:18:49 +0200

    16/11/17

--- demo.py
+++ demo.py
@@ -1,6 +1,11 @@
 
 import platform
-import sys, tty, termios
+import sys
+import copy as copy
+import os
+
+
+__author__ = 'Pavlidis Georgios'
 
 Total_nodes = 0
 initial_nodes = []
@@ -21,75 +26,146 @@ def readFile(importFile):
 	with open(importFile,"r+") as file:
 		#total number of nodes
 		Total_nodes = int(file.readline())
+		#initialize edges
+		for i in range (0, Total_nodes):
+			edges.append([])
+			initial_nodes.append(0)
+			final_nodes.append(0)
 
-		#read the starting nodes
-		for x in file.readline().split(' '):
-			initial_nodes.append(int(x))
-
-		#validity check
-		for node in initial_nodes:
-			if node<0 or node>=Total_nodes:
-				print( "you have problem with your INITIAL nodes, please check it and try again!")
-				return False
+		try:	
+			#read the starting nodes
+			for x in file.readline().split(' '):
+				initial_nodes[int(x)]=1
+		except:
+			print( "you have problem with your INITIAL nodes, please check it and try again!")
+			return False
 
 
 		#read the final nodes
-		for x in file.readline().split(' '):
-			final_nodes.append(int(x))
+		try:	
+			#read the starting nodes
+			for x in file.readline().split(' '):
+				final_nodes[int(x)]=1
+		except:
+			print( "you have problem with your FINAL nodes, please check it and try again!")
+			return False
 
-		#validity check
-		for node in final_nodes:
-			if node<0 or node>=Total_nodes:
-				print( "you have problem with your FINAL nodes, please check it and try again!")
-				return False
 
 
 		#read the edges of this model
-		# the numbers of nodes are represented as integer	
+		# the numbers of {GP} nodes are represented as integer	
 		# and conditions are represented as string
 		for line in file:
 			edge = line[:len(line)-1].split(' ')
 			x = []
 			try:
-				x.append(int(edge[0]))
+
 				x.append(edge[1])
 				x.append(int(edge[2]))
 			except:
 				print ("You have problem with edge at line", line)
 				return False
 
-			edges.append(x)
+			edges[int(edge[0])].append(x)
 
 		return True
 
 
-def model():
-	print ('Press <ENTER> to begin:\n')
-	input_char='s'
-	stri = ""
+#GP read char from terminal without press the <ENTER> 
+def read_char():
 	if 'Windows' == platform.system():
+		import msvcrt
 		input_char = getch.getch()
 
 	else:
-		while not input_char.upper() == chr(13):
-			fd = sys.stdin.fileno()
-			old_settings = termios.tcgetattr(fd)
-			try:
-				tty.setraw(sys.stdin.fileno())
-				input_char = sys.stdin.read(1)
-				print input_char,
-				stri+=input_char
-			finally:
-				termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+		import tty, termios
+		fd = sys.stdin.fileno()
+		old_settings = termios.tcgetattr(fd)
+		try:
+			tty.setraw(sys.stdin.fileno())
+			input_char = sys.stdin.read(1)
+			print input_char,
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
-	print stri
-	if input_char.upper() == chr(13): 
-		print ('YES')
+	return input_char
+
+
+
+
+
+
+def check(current_nodes, input_char):
+	while True:
+
+		new_current_nodes=[]
+		#for each current node..
+		for node in current_nodes:
+
+			# for each edge of this node..
+			for x in edges[node]:
+			
+				#check if the input char is condition for this edge 
+				#or if there is the empty word
+			 	if x[0]==input_char or x[0]=='@':
+			 		if(new_current_nodes.count(x[1])==0):
+			 			new_current_nodes.append(x[1])
+
+		#GP if the new and the old list are the same it mean that all pollible and suitable nodes are in there
+		if(not current_nodes==new_current_nodes):
+			current_nodes = new_current_nodes
+		else:
+			return current_nodes
+
+
+
+def model():
+	global current_nodes
+	print ('Press <ENTER> to begin:\n')
+	input_char='s'
+
+	#GP initialize the current nodes
+	for i in range(0,Total_nodes):
+		if initial_nodes[i]==1:
+			current_nodes.append(i)
+		 	for x in edges[i]: 
+			 	if x[0]=='@':
+			 		if(current_nodes.count(x[1])==0):
+			 			current_nodes.append(x[1])
+
+
+	print current_nodes
+	while True:
+
+		#read the next character
+		input_char= read_char()
+		
+		#repeat until <ENTER> be pressed
+		if input_char ==chr(13):
+			break
+
+		#create the new list of current nodes
+		current_nodes = check(current_nodes, input_char)
+
+		#if threre are not other node, break.
+		if(current_nodes==[]):
+			break
+		#print current_nodes,
+
+	
+	for node in current_nodes:
+		if final_nodes[node]==1:
+			print ('\nYES')
+			return True
+	print ('\nNO')
+	return False
+		
 
 
 def print_des():
-		print(" ")
-		print ("The description must follow the format bellow ")
+		print("GP_15112")
+		os.system("clear")
+		print ("The description must follow the format bellow: ")
 		print(" ")
 		print ("	line 1: Total_number_of_nodes")
 		print ("	line 2: initial_node")
@@ -97,12 +173,14 @@ def print_des():
 		print ("	line 4 and bellow: edges as ")
 		print ("		[source_node, 'conditions', destination_node]:")
 		print ("			split with <SPACE>")
+		print ("	last line: empty line (very important!!!)")
 		print ("Example: ")
 		print ("	line 1: 3 ")
 		print ("	line 2: 0 ")
 		print ("	line 3: 1 2 ")
 		print ("	line 4: 0 a 1 ")
 		print ("	line 5: 0 b 2 ")
+		print ("	line 6: ")
 
 
 if __name__ == "__main__":
@@ -110,8 +188,8 @@ if __name__ == "__main__":
 	print_des()
 	while(not flag):
 
-		#file = raw_input ("Entrer the name of file with description of model: ")
-		file = "example1.txt"
+		file = raw_input ("Entrer the name of file with description of model: ")
+		#file = "example1.txt"
 		try:
 			flag = readFile(file)
 			if not flag: 
@@ -122,9 +200,12 @@ if __name__ == "__main__":
 		
 
 
-		print ("Total_nodes ", Total_nodes)
-		print ("initial_nodes ", initial_nodes)
-		print ("final_nodes ", final_nodes)
-		print ("edges", edges) 
-		#sys.argv[1]
-		model()
+		#print ("Total_nodes ", Total_nodes)
+		#print ("initial_nodes ", initial_nodes)
+		#print ("final_nodes ", final_nodes)
+		#print ("edges", edges) 
+		while True:
+			model()
+			k = raw_input ("Would you like to try again ??(Y/N) ")
+			if ( k.upper()=='N'):
+				break
